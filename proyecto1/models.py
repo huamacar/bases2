c82e83da5a42f94e16cb24cd52f1c7bc30b90f93
andrea <dry.lez@gmail.com>
2015-08-21 14:58:10 -0700

    agregando clase afiliado en models

--- proyecto1/models.py
+++ proyecto1/models.py
@@ -7,3 +7,8 @@ class Usuario(models.Model):
     class Meta:
         db_table = 'Usuario'
 
+class Afiliado(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'Afiliado'
